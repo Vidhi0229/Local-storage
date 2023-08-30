d01745c599c122cd3f0fd44e9f75896243288466
Vidhi <srivastavavidhi29@gmail.com>
2023-08-30 10:25:02 +0530

    implemented setItem, getItem and removeItem

---
+++ script.js
@@ -0,0 +1,8 @@
+localStorage.setItem('name', 'Vidhi')
+localStorage.setItem('college', 'AKGEC')
+
+let names = localStorage.getItem('name')
+let college = localStorage.getItem('college')
+
+localStorage.removeItem('college')
+
