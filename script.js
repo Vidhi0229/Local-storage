8adffdaa472da226798f57a3abb77b15af8dc344
Vidhi <srivastavavidhi29@gmail.com>
2023-08-30 11:01:48 +0530

    stringfying and parsing data

--- script.js
+++ script.js
@@ -1,8 +1,27 @@
+localStorage.clear()
+
+// localStorage.setItem('name', 'Vidhi', 'age', 23)
+
+// let names = localStorage.getItem('name')
+// let age = localStorage.getItem('age')
+
+//console.log(names, age);
+
+//localStorage.clear()
+
 localStorage.setItem('name', 'Vidhi')
-localStorage.setItem('college', 'AKGEC')
 
 let names = localStorage.getItem('name')
-let college = localStorage.getItem('college')
+//let age = localStorage.getItem('age')
 
-localStorage.removeItem('college')
 
+const todos = [
+    {name: "Vidhi", age: 23},
+    {name: "Nancy", age: 24},
+    {name: "Khushboo", age: 22}
+];
+
+localStorage.setItem('todos', JSON.stringify(todos))
+
+const item = localStorage.getItem('todos')
+console.log(JSON.parse(item));
